bcace4d8ddad3afec8cf5b05b52268ec57eded6c
Purvit <pamehta50@gmail.com>
2025-04-30 09:52:23 +0530

    Update for locust_mcp

--- tests/generated/fake_api_test.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, between
-
-class FakeApiUser(HttpUser):
-    host = "https://fake-json-api.mock.beeceptor.com"
-    wait_time = between(1, 2)  # Random wait between requests
-
-    @task
-    def get_users(self):
-        self.client.get("/users", verify=False)  # Disable SSL verification for testing
